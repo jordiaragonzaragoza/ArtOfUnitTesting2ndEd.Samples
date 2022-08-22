000b3d6983f7e689aeb43306885ac8a02318783f
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2022-08-22 19:46:01 +0200

    Initial Commit. Till Chapter2.

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,112 @@
+﻿namespace LogAn.UnitTests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            // Arrange : Init the environment.
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            // Act : Method to test.
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+
+            Assert.True(result);
+        }
+
+        // this is a refactoring of the previous two tests
+        [TestCase("filewithgoodextension.SLF")]
+        [TestCase("filewithgoodextension.slf")]
+        public void IsValidLogFileName_ValidExtensions_ReturnsTrue(string file)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.True(result);
+        }
+
+
+        // this is a refactoring of all the "regular" tests
+        [TestCase("filewithgoodextension.SLF", true)]
+        [TestCase("filewithgoodextension.slf", true)]
+        [TestCase("filewithbadextension.foo", false)]
+        public void IsValidLogFileName_VariousExtensions_ChecksThem(string file, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_Throws()
+        {
+            LogAnalyzer la = new LogAnalyzer();
+
+            var ex = Assert.Throws<ArgumentException>(() => la.IsValidLogFileName(""));
+
+            StringAssert.Contains("filename has to be provided", ex.Message);
+        }
+
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_ThrowsFluent()
+        {
+            LogAnalyzer la = new LogAnalyzer();
+
+            var ex = Assert.Throws<ArgumentException>(() => la.IsValidLogFileName(""));
+
+            Assert.That(ex.Message, Does.Contain("filename has to be provided"));
+        }
+
+        [Test]
+        public void IsValidLogFileName_WhenCalled_ChangesWasLastFileNameValid()
+        {
+            LogAnalyzer la = new LogAnalyzer();
+
+            la.IsValidLogFileName("badname.slf");
+
+            Assert.IsTrue(la.WasLastFileNameValid);
+        }
+
+        //refactored from above
+        [TestCase("badfile.foo", false)]
+        [TestCase("goodfile.slf", true)]
+        public void IsValidLogFileName_WhenCalled_ChangesWasLastFileNameValid(string file, bool expected)
+        {
+            LogAnalyzer la = new LogAnalyzer();
+
+            la.IsValidLogFileName(file);
+
+            Assert.That(la.WasLastFileNameValid, Is.EqualTo(expected));
+        }
+
+    }
+}
+
---
+++ LogAn.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class MemCalculatorTests
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calc = MakeCalc();
+
+            int lastSum = calc.Sum();
+
+            Assert.That(lastSum, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator calc = MakeCalc();
+
+            calc.Add(1);
+            int sum = calc.Sum();
+
+            Assert.That(sum, Is.EqualTo(1));
+        }
+
+        private static MemCalculator MakeCalc()
+        {
+            return new MemCalculator();
+        }
+    }
+}
